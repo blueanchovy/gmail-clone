e9c49f31b460595cae09582a10be8216a0b28673
Manish Jha <manishjhazyx@gmail.com>
2022-01-26 20:50:44 +0530

    created header

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,12 @@ import React from "react";
 import "./Header.css";
 import MenuIcon from "@mui/icons-material/Menu";
 import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import TuneIcon from "@mui/icons-material/Tune";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import AppsRoundedIcon from "@mui/icons-material/AppsRounded";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import Avatar from "@mui/material/Avatar";
 
 function Header() {
   return (
@@ -15,12 +21,35 @@ function Header() {
           </div>
         </div>
 
-        <img src="https://cdn-icons-png.flaticon.com/512/5968/5968534.png" />
+        <img src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_2x_r2.png" />
       </div>
 
-      <div className="header__middle"></div>
+      <div className="header__middle">
+        <SearchIcon />
+        <input
+          className="header__searchText"
+          type="text"
+          placeholder="Search Mail"
+        ></input>
+        <IconButton>
+          <TuneIcon />
+        </IconButton>
+      </div>
 
-      <div className="header__right"></div>
+      <div className="header__right">
+        <IconButton>
+          <HelpOutlineOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <AppsRoundedIcon />
+        </IconButton>
+        <IconButton>
+          <Avatar />
+        </IconButton>
+      </div>
     </div>
   );
 }
