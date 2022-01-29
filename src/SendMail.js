f70311a839ae553f170c5436fc311418fba41971
Manish Jha <manishjhazyx@gmail.com>
2022-01-29 21:47:30 +0530

    added mail sendbox

---
+++ src/SendMail.js
@@ -0,0 +1,86 @@
+import React from "react";
+import "./SendMail.css";
+import CloseIcon from "@mui/icons-material/Close";
+import { Button } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { closeSendMessage } from "./features/mailSlice";
+import { db } from "./firebase";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+
+function SendMail() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (formData) => {
+    console.log(formData);
+    addDoc(collection(db, "emails"), {
+      to: formData.to,
+      subject: formData.subject,
+      message: formData.message,
+      timestamp: serverTimestamp(),
+    });
+
+    dispatch(closeSendMessage());
+  };
+
+  const dispatch = useDispatch();
+
+  return (
+    <div className="sendMail">
+      <div className="sendMail__header">
+        <h3>New Message</h3>
+        <CloseIcon
+          className="sendMail__close"
+          onClick={() => dispatch(closeSendMessage())}
+        />
+      </div>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name="to"
+          placeholder="To"
+          type="email"
+          {...register("to", { required: true })}
+        />
+        {errors.to && <p className="sendMail__error">Recipient is required</p>}
+        <input
+          name="subject"
+          placeholder="Subject"
+          type="text"
+          {...register("subject", { required: true })}
+        />
+        {errors.subject && (
+          <p className="sendMail__error">Subject is required</p>
+        )}
+        <input
+          name="message"
+          placeholder="Message..."
+          type="text"
+          {...register("message", { required: true })}
+          className="sendMail__message"
+        />
+        {errors.message && (
+          <p className="sendMail__error">Message is required</p>
+        )}
+
+        <div className="sendMail__options">
+          <Button
+            className="sendMail__send"
+            variant="contained"
+            color="primary"
+            type="submit"
+          >
+            Send
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default SendMail;
