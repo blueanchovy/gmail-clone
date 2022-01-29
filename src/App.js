d8825131f6149aa93995e20c9e09af6cc727b472
Manish Jha <manishjhazyx@gmail.com>
2022-01-29 21:48:05 +0530

    minor edit

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
           login({
             displayName: user.displayName,
             email: user.email,
-            photoUrl: user.photoUrl,
+            photoUrl: user.photoURL,
           })
         );
       } else {
